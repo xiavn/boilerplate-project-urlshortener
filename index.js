9b4ef92c9c7795cb14f36e64720254dac9b15f6e
Lianne Turnbull <lianne.turnbull@gmail.com>
2022-09-09 19:03:08 +0100

    Set up url shorterner with database

--- index.js
+++ index.js
@@ -4,14 +4,23 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const dns = require("dns");
 const { URL } = require("url");
+const mongoose = require("mongoose");
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const urlSchema = new mongoose.Schema({
+    url: { type: String, required: true },
+});
+
+const UrlModel = mongoose.model("Url", urlSchema);
 
 const urlEncodedParser = bodyParser.urlencoded({ extended: false });
-
-const shortUrls = [];
 
 app.use(cors());
 
@@ -22,26 +31,37 @@ app.get("/", function (req, res) {
 });
 
 // Your first API endpoint
-app.post("/api/shorturl", urlEncodedParser, function (req, res) {
+app.post("/api/shorturl", urlEncodedParser, async (req, res) => {
     const url = req.body.url;
-    const urlObject = new URL(url);
-    dns.lookup(urlObject.hostname, (err) => {
-        console.log(err);
+    const saveUrl = async () => {
+        try {
+            const urlModel = new UrlModel({ url });
+            const savedUrl = await urlModel.save();
+            res.json({
+                original_url: url,
+                short_url: savedUrl.get("_id"),
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    await dns.lookup(new URL(url).hostname, async (err) => {
         if (err) {
             res.json({ error: "invalid url" });
         } else {
-            shortUrls.push(req.body.url);
-            res.json({
-                original_url: req.body.url,
-                short_url: shortUrls.length - 1,
-            });
+            await saveUrl();
         }
     });
 });
 
-app.get("/api/shorturl/:shortUrl", function (req, res) {
-    const fullUrl = shortUrls[Number(req.params.shortUrl)];
-    res.redirect(fullUrl);
+app.get("/api/shorturl/:shortUrl", async (req, res) => {
+    try {
+        const url = await UrlModel.findById(req.params.shortUrl);
+        const fullUrl = url.get("url");
+        res.redirect(fullUrl);
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 app.listen(port, function () {
